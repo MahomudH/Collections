55382c05e5ed7164d79ae344d8e53e3ce3e4e8fb
Mahmoud Hosni <mah1998mod@gmail.com>
2023-09-01 21:39:01 +0300

    added hashset and sortedset example

---
+++ Collections.HashSetAndSortedSetApp/Customer.cs
@@ -0,0 +1,53 @@
+﻿namespace Collections.HashSetAndSortedSetApp
+{
+    public class Customer : IComparable<Customer>
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+
+        public override int GetHashCode()
+        {
+            var hash = 17;
+            hash = hash * 23 + Phone.GetHashCode();
+
+            return hash;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            var customer = obj as Customer;
+            if (customer is null)
+                return false;
+
+            return this.Phone.Equals(customer.Phone);
+        }
+
+        public static bool operator ==(Customer customer1, Customer customer2)
+        {
+            if (customer1 is null || customer2 is null)
+                return false;
+
+            return customer1.Phone.Equals(customer2.Phone);
+        }
+
+        public static bool operator !=(Customer customer1, Customer customer2)
+        {
+            if (customer1 is null || customer2 is null)
+                return false;
+
+            return !customer1.Phone.Equals(customer2.Phone);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} ({this.Phone})";
+        }
+
+        public int CompareTo(Customer other)
+        {
+            if(object.ReferenceEquals(this, other)) return 0 ;
+            if(other is null) return -1 ;
+            return this.Name.CompareTo(other.Name); 
+        }
+    }
+}
---
+++ Collections.HashSetAndSortedSetApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using Collections.HashSetAndSortedSetApp;
+Console.WriteLine("----------");
+Console.WriteLine("HashSet");
+Customer[] customers = new Customer[] 
+{
+    new Customer { Name = "Mahmoud", Phone = "+972 059 1546 824" }, 
+    new Customer { Name = "Ahmed", Phone = "+972 059 1546 823" }, 
+    new Customer { Name = "Mahmoud", Phone = "+972 059 1546 821" }, 
+    new Customer { Name = "Shatha", Phone = "+972 059 1546 824" }, // this will not added to hashset because she has the same mahmouds' phone number 
+    new Customer { Name = "Alaa", Phone = "+972 059 1546 829" }, 
+};
+
+HashSet<Customer> customerSet = new HashSet<Customer>(customers);
+
+foreach (Customer customer in customerSet) Console.WriteLine(customer);
+
+Console.WriteLine("----------");
+Console.WriteLine("SortedSet");
+
+// the sortedset will sort the customer by their name because I make the "CompareTo" function compare by "Name"  
+SortedSet<Customer> customerSortedSet = new SortedSet<Customer>(customers);
+
+foreach (Customer customer in customerSortedSet) Console.WriteLine(customer);
